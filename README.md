6f872ab6decf53fdac3d02eb1753ffa12493a65f
ktzanev <kroum.tzanev@univ-lille.fr>
2022-06-05 18:56:58 +0200

    transformation en PWA

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 Le logiciel Apogée, utilisé à l'université de Lille pour la gestion des notes des étudiants, peut exporter et importer au format (quasi)`tsv` (comme `csv`, mais le séparateur est le caractère tabulation). Malheureusement ce fichier contient beaucoup trop d'informations et est très peu tolérant aux erreurs humaines.
 
-Ce logiciel (page web) permet de convertir ces fichiers dans un format « lisible » (`csv` avec seulement 4 colonnes `Num`, `NOM`, `PRENOM`, `Note`).
+Ce logiciel (page web) permet de convertir ces fichiers dans un format « lisible » (`csv` avec seulement 4 colonnes `Num`, `NOM`, `PRENOM`, `Note`). Et vice-versa.
+
+# Sécurité
+
+Tous les calculs sont effectués dans le navigateur. Aucune information n'est envoyée sur un serveur. Il s'agit d'une page [PWA](https://fr.wikipedia.org/wiki/Progressive_web_app) qui peut être installée et executée sans connexion internet.
 
 # Licence
 
---
+++ pwa_sw.js
@@ -0,0 +1,51 @@
+// Choose a cache name
+const cacheName = 'apogie-v1';
+// List the files to precache
+const precacheResources = [
+  '/apogie/index.html',
+  '/apogie/lisezmoi.js',
+  '/apogie/main.css',
+  '/apogie/main.js',
+  'images/apogie72.png',
+  'images/apogie96.png',
+  'images/apogie128.png',
+  'images/apogie192.png',
+  'images/apogie384.png',
+  'images/apogie512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.22/vue.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.0/jszip.min.js',
+];
+
+// When the service worker is installing, open the cache and add the precache resources to it
+self.addEventListener('install', (event) => {
+  console.log('Service worker install event!');
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service worker activate event!');
+  event.waitUntil(
+    caches.keys().then(function(allCaches) {
+      return Promise.all(
+        allCaches.filter(function(name) {
+          return name != cacheName
+        }).map(function(name) {
+          return caches.delete(name);
+        })
+      );
+    })
+  );
+});
+
+// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
+self.addEventListener('fetch', (event) => {
+  console.log('Fetch intercepted for:', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      // Cache first strategy
+      return cachedResponse || fetch(event.request);
+    }),
+  );
+});
